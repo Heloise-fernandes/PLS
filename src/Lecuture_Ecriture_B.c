d4389d67d7d48bab0ac946ba470f923b70fd8813
RomaneGarou <romanegallier@gmail.com>
2016-05-26 16:26:59 +0200

    m

--- src/Lecuture_Ecriture_B.c
+++ src/Lecuture_Ecriture_B.c
@@ -43,7 +43,7 @@ void fermetureFichier(FILE* fichier)
  * */
 int getByte(FILE* fichier, char *octet)
 {
-	int tailleLu = 0;
+	int tailleLu;
 	if(fichier==NULL){printf("Le fichier n'est pas ouvert\n"); exit(0);}
 	tailleLu = fread(octet,sizeof(char),1,fichier);
 	return tailleLu;
@@ -69,34 +69,43 @@ void putByte(FILE* fichier,char octet)
 void putBit(FILE* fichier,char bit, int indice)
 {
 	char octet = 0;
-	int tailleLU;
+	int tailleLu;
 	
 	if(fichier==NULL){printf("Le fichier n'est pas ouvert\n"); exit(0);}
+	if((bit != 1)&&(bit != 0)){printf("On veux un bit\n"); exit(0);}
 	
-	tailleLU = getByte(fichier, &octet);//On recupere l'octet pointé par le curseur
-	if(tailleLU != 1)//Nous sommes à la a fin du fichier il faut donc ajouter un octet
+	tailleLu = getByte(fichier, &octet);//On recupere l'octet pointé par le curseur
+	
+	printf("La taille lu est : %d\n",tailleLu);
+	
+	bit = bit<<indice;
+	
+	if(tailleLu != 1)//On va ajouter un nouvel octet
 	{
-		putByte(fichier, bit<<(INDICE_MAX-indice));//On ajoute l'octet
+		printf("On ajoute un nouvel octet vierge : %d\n",bit);
+		putByte(fichier, bit);//On ajoute l'octet
 	}
 	else
-	{
-		tailleLU = 1;
-		bit = bit<<(INDICE_MAX-indice);
-		switch(indice)//On ajoute e bit à l'octet
+	{		
+		printf("On recupere l'octet %d\n",octet);		
+
+		switch(indice)//On ajoute le bit à l'octet
 		{
-			case 0 : octet = (octet&(~BIT_7))&bit; break;
-			case 1 : octet = (octet&(~BIT_6))&bit; break;
-			case 2 : octet = (octet&(~BIT_5))&bit; break;
-			case 3 : octet = (octet&(~BIT_4))&bit; break;
-			case 4 : octet = (octet&(~BIT_3))&bit; break;
-			case 5 : octet = (octet&(~BIT_2))&bit; break;
-			case 6 : octet = (octet&(~BIT_1))&bit; break;
-			case 7 : octet = (octet&(~BIT_0))&bit; break;
+			case 0 : octet = (octet&(~BIT_0))+bit; break;
+			case 1 : octet = (octet&(~BIT_1))+bit; break;
+			case 2 : octet = (octet&(~BIT_2))+bit; break;
+			case 3 : octet = (octet&(~BIT_3))+bit; break;
+			case 4 : octet = (octet&(~BIT_4))+bit; break;
+			case 5 : octet = (octet&(~BIT_5))+bit; break;
+			case 6 : octet = (octet&(~BIT_6))+bit; break;
+			case 7 : octet = (octet&(~BIT_7))+bit; break;
 			default:printf("L'indice est faux!!!!!\n"); exit(0);
 		}
-		putByte(fichier, octet);//On ecrit l'octet avec le bit ajouté		
-		if((indice>=0)&&(indice<7)){fseek(fichier,-1,SEEK_CUR);}//Si tout l'octet n'a pas été lu(le bit d'indice 7 n'as pas été lu) on repositionne le curseur sur l'octet en cours de lecture	
+		printf("On ajoute le bit %d\n",bit);
+		putByte(fichier, octet);//On ecrit l'octet avec le bit ajouté			
 	}
+	if((indice>=0)&&(indice<7)){printf("Bit courant, %d ---",SEEK_CUR);fseek(fichier,-1,SEEK_CUR);printf("Retour au bit precedent, %d\n",SEEK_CUR);}
+	//Si tout l'octet n'a pas été lu(le bit d'indice 7 n'as pas été lu) on repositionne le curseur sur l'octet en cours de lecture	
 	
 }
 
@@ -106,14 +115,16 @@ void putBit(FILE* fichier,char bit, int indice)
  * indice : bit à lire, il doit être compris en 0 et 7
  * bit : le bit lu
  * */
-int getBit(FILE* fichier, int indice, char *bit)
+int getBit(FILE* fichier, int *indice, char *bit)
 {
-	char *octet;
+	char *octet = malloc(sizeof(char));
 	int tailleLU;
+	
 	
 	if(fichier==NULL){printf("Le fichier n'est pas ouvert\n"); exit(0);}
 	
 	tailleLU = getByte(fichier, octet);//On recupere l'octet pointé par le curseur
+	
 	
 	if(tailleLU != 1)
 	{
@@ -123,7 +134,7 @@ int getBit(FILE* fichier, int indice, char *bit)
 	else
 	{
 		tailleLU = 1;
-		switch(indice)
+		switch(*indice)
 		{
 			case 0 : *bit = (*octet&BIT_7); break;
 			case 1 : *bit = (*octet&BIT_6); break;
@@ -135,12 +146,85 @@ int getBit(FILE* fichier, int indice, char *bit)
 			case 7 : *bit = (*octet&BIT_0); break;
 			default:printf("L'indice est faux!!!!!\n"); exit(0);
 		}
-		*bit = *bit>>(INDICE_MAX-indice);
-		if((indice>=0)&&(indice<7)){fseek(fichier,-1,SEEK_CUR);}//Si tout l'octet n'a pas été lu(le bit d'indice 7 n'as pas été lu) on repositionne le curseur sur l'octet en cours de lecture	
+		*bit = *bit>>(INDICE_MAX-(*indice));
+		if((*indice>=0)&&(*indice<7)){fseek(fichier,-1,SEEK_CUR);}//Si tout l'octet n'a pas été lu(le bit d'indice 7 n'as pas été lu) on repositionne le curseur sur l'octet en cours de lecture	
 	}
+	*indice = (*indice)+1;
 	return tailleLU;
 }
 
-void putInt (FILE* F,int t){
+
+int main(int argc, char **argv)
+{
 	
+	if(argc==3)
+	{
+		char car = 0;
+		int taille;
+		int i;
+		/*FILE* fichier = ouvertureFichierLecture(argv[1]);
+		taille = getByte(fichier,&car);
+		printf("Nous avons lu un caractère de %d octet qui est %d\n",taille,car);//a
+		taille = getByte(fichier,&car);
+		printf("Nous avons lu un caractère de %d octet qui est %d\n",taille,car);//2
+		taille = getByte(fichier,&car);
+		printf("Nous avons lu un caractère de %d octet qui est %d\n",taille,car);//5
+		taille = getByte(fichier,&car);
+		printf("Nous avons lu un caractère de %d octet qui est %d\n",taille,car);//b
+		taille = getByte(fichier,&car);
+		printf("Nous avons lu un caractère de %d octet qui est %d\n",taille,car);//\n
+		taille = getByte(fichier,&car);
+		printf("Nous avons lu un caractère de %d\n",taille);//rien
+		fermetureFichier(fichier);
+		*/
+		FILE* fichier = ouvertureFichierLecture(argv[1]);
+		int *indice = 0;
+		for(i=0; i<=14; i++)
+		{
+			if(i%8==0){printf("===========================================\n");}
+			*indice = i%8;
+			taille = getBit(fichier,indice,&car);
+			printf("Taille :  %d octet / Valeur : %d\n",taille,car);
+			
+		}
+		printf("===========================================\n");
+		taille = getByte(fichier,&car);
+		printf("Nous avons lu un caractère de %d octet qui est %d\n",taille,car);
+		printf("===========================================\n");
+		for(i=0; i<=7; i++)
+		{
+			*indice = i%8;
+			taille = getBit(fichier,indice,&car);
+			printf("Taille :  %d octet / Valeur : %d\n",taille,car);
+			
+		}
+		fermetureFichier(fichier);
+		
+		fichier = ouvertureFichierEcriture(argv[2]);
+		putByte(fichier,90);
+		fermetureFichier(fichier);
+		
+		printf("===========================================\n");
+		char blop = 1;
+		fichier = ouvertureFichierEcriture(argv[2]);
+		putBit(fichier,blop,0);
+		putBit(fichier,blop,6);
+		fermetureFichier(fichier);
+		fichier = ouvertureFichierLecture(argv[2]);
+		printf("===========================================\n");
+		for(i = 0; i<=7; i++)
+		{
+			*indice = i%8;
+			taille = getBit(fichier,indice,&car);
+			if(taille!=0)
+			{
+				printf("Nous avons lu un bit de %d octet qui est %d\n",taille,car);
+			}
+			else{break;}
+		}
+		fermetureFichier(fichier);
+	}
+	
+	
+	return 0;
 }
