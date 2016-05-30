b82a412eb27cb52c276d58961a06026a0db2a749
ArchibaldLeMagnifique <denis.lachartre@free.fr>
2016-05-30 22:21:32 +0200

    presque marche

--- pretraitement/MTF.c
+++ pretraitement/MTF.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void initialiser_tab(unsigned char* tab){
+void initialiser_tab(char* tab){
 	int i;
 	for(i=0;i<256;i++){
-		tab[i]=i;
+		tab[i]=i-128;
 	}
 }
 
-void MTF(FILE* fichier_src){
-	FILE* fichier_dest = ouvertureFichierEcriture("apres_MTF");
+void MTF(FILE* fichier_src,FILE* fichier_dest){
 	
-	unsigned char tab[256];
+	char tab[255];
 	initialiser_tab(tab);
 	char octet_lu,v;
 	int taille_octet=getByte(fichier_src,&octet_lu);
@@ -19,46 +18,44 @@ void MTF(FILE* fichier_src){
 
 
 	while(taille_octet == 1){
-		
+			
 		i=0;
 		while((tab[i]!=octet_lu) & (i<256)){
 			i+=1;
 		}
 		v=tab[i];
 		
-		
 		for(j=0;j<i;j++){
 			tab[i-j]=tab[i-j-1];
 		}
 		tab[0]=v;
-		putByte(fichier_dest,(unsigned char)(i));
+		putByte(fichier_dest,(char)(i-128));
 		taille_octet = getByte(fichier_src,&octet_lu);
 	}
-	fermetureFichier(fichier_dest);
 }
 
 
-void unMTF(FILE* fichier_src){
-	FILE* fichier_dest = ouvertureFichierEcriture("apres_unMTF");
+void unMTF(FILE* fichier_src,FILE* fichier_dest){
 
-	unsigned char tab[256];
+	char tab[255];
 	initialiser_tab(tab);
 	char octet_lu,v;
 	int taille_octet=getByte(fichier_src,&octet_lu);
-	int i,n;
-	printf("%d ",taille_octet);
+	int i,j,n;
+	
 	while(taille_octet == 1){
-		
 
-		n=octet_lu;//indice de l'element a MTF
-		v=tab[n];//element a MTV
+		n=octet_lu+128;//indice de l'element a MTF
+		v=tab[n];//element a MTF
 		
-		for(i=0;i<n;i++){
-			tab[n-i]=tab[n-i-1];
+		i=0;
+		while(tab[i]!=v){i+=1;}
+		for(j=0;j<i;j++){
+			tab[i-j]=tab[i-j-1];
 		}
-		tab[0]=(unsigned char)(v);
-		putByte(fichier_dest,(unsigned char)(v));
+		
+		tab[0]=(char)(v);
+		putByte(fichier_dest,(char)(v));
 		taille_octet = getByte(fichier_src,&octet_lu);
 	}
-	fermetureFichier(fichier_dest);
 }
